4c86d63e8e4d73e597bd038aa7bc27d8dbba74ec
MikhailArpishkin <mixail.arpishkin@yandex.ru>
2023-06-15 22:18:36 +0300

    test4

--- app/task/blueprint.py
+++ app/task/blueprint.py
@@ -34,7 +34,7 @@ def task_create():
 @login_required
 def task_search():
     user_input = request.get_json().get('kr_name')
-    sql = text(f'SELECT * FROM iss WHERE kr_name = {user_input}')
+    sql = text(f'SELECT * FROM iss WHERE kr_name = {user_input};')
     q = db.session.execute(sql)
     #q = models.iss.query.filter(models.iss.kr_name == user_input).all()
     return jsonify(q)
