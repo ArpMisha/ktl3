35cd22bf3ea46b8e2a05127cbfc71d17bd619cde
MikhailArpishkin <mixail.arpishkin@yandex.ru>
2023-06-28 17:08:22 +0300

    www20

---
+++ app/static/js/app3.js
@@ -0,0 +1,28 @@
+const { createApp } = Vue 
+
+const TaskApp = {
+  data(){
+    return {
+      search: {
+      'name': ''
+      },
+      tasks: []
+    }
+  },
+  methods: {
+    async searchTask(){
+      const response = await fetch(window.location + '_search', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Requested-With': 'XMLHttpRequest'
+        },
+        body: JSON.stringify(this.search)
+      })
+      this.tasks = await response.json()
+    }
+  },
+  delimiters: ['{', '}']
+}
+
+createApp(TaskApp).mount('#app3')
--- app/task3/blueprint.py
+++ app/task3/blueprint.py
@@ -13,13 +13,16 @@ task3 = Blueprint('task3', __name__, template_folder='templates')
 @task3.route('/task3')
 #@login_required
 def index():
-    #hostname = request.values.get(hostname)
-    cmd = 'ls '
-    resualt = subprocess.check_output(cmd, shell=True)
-    return str(type(subprocess.check_output(cmd, shell=True)))
-    #return render_template('task3/index.html', resualt=resualt)
+    return render_template('task3/index.html')
 
-
+@task3.route('/task3_search', methods=['POST'])
+#@login_required
+def task3_search():
+    user_input = request.get_json().get('name')
+    cmd = 'cat' + user_input
+    results = subprocess.check_output(cmd, shell=True)
+    d = {"info": results}
+    return jsonify(d)
 
 
 
