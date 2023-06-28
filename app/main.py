70ae575f302da55bcfc8f9f4f1b03b9c0da79aff
MikhailArpishkin <mixail.arpishkin@yandex.ru>
2023-06-28 16:37:58 +0300

    www16

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@ from app import app
 import view 
 from task.blueprint import task
 from task2.blueprint import task2
+from task3.blueprint import task3
 
 app.register_blueprint(task)
 app.register_blueprint(task2)
+app.register_blueprint(task3)
 
 if __name__ == '__main__':
 	app.run(host = '0.0.0.0')
---
+++ app/task3/blueprint.py
@@ -0,0 +1,42 @@
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
+from flask_security import login_required
+import models 
+from app import db
+from sqlalchemy import text
+from datetime import datetime
+import subprocess
+
+
+
+task3 = Blueprint('task3', __name__, template_folder='templates')
+
+@task3.route('/task3')
+#@login_required
+def index():
+    hostname = request.values.get(hostname)
+    cmd = 'nslookup ' + hostname
+    resualt = subprocess.check_output(cmd, shell=True)
+    #return subprocess.check_output(cmd, shell=True)
+    return render_template('task3/index.html', resualt=resualt)
+
+
+
+
+
+
+
+  
+
+
+
+
+   
+
+   
+
+
+
+
+
+
+    
