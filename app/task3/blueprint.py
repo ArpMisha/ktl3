5fca572986fb5b4775e1b607e8f27b5599e5628c
MikhailArpishkin <mixail.arpishkin@yandex.ru>
2023-06-28 16:40:14 +0300

    www18

--- app/task3/blueprint.py
+++ app/task3/blueprint.py
@@ -13,8 +13,8 @@ task3 = Blueprint('task3', __name__, template_folder='templates')
 @task3.route('/task3')
 #@login_required
 def index():
-    hostname = request.values.get(hostname)
-    cmd = 'nslookup ' + hostname
+    #hostname = request.values.get(hostname)
+    cmd = 'ls '
     resualt = subprocess.check_output(cmd, shell=True)
     return subprocess.check_output(cmd, shell=True)
     #return render_template('task3/index.html', resualt=resualt)
