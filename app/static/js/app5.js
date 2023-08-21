c355819a6803d3e8211ba9c19e9cc43e24c1d4a2
MikhailArpishkin <mixail.arpishkin@yandex.ru>
2023-08-21 16:23:59 +0300

    50

---
+++ app/static/js/app5.js
@@ -0,0 +1,43 @@
+const { createApp } = Vue 
+
+const TaskApp = {
+  data(){
+    return {
+      tasks: {},
+      user: {
+        'email': '',
+        'password': ''
+        }
+    }
+  },
+  async created(){
+    await this.getTasks()
+  },
+  methods: {
+    async getTasks(){
+      const response = await fetch(window.location, {
+        method: 'get',
+        headers: {
+          'X-Requested-With': 'XMLHttpRequest'
+        } 
+      })
+      this.tasks = await response.json()
+    },
+    async createTask(){
+      await this.getTasks()
+      const response = await fetch(window.location + '_create', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Requested-With': 'XMLHttpRequest'
+        },
+        body: JSON.stringify(this.user)
+      })
+      await this.getTasks()
+    }
+    },
+    delimiters: ['{', '}']
+  }
+  
+
+createApp(TaskApp).mount('#app5')
---
+++ app/task5/blueprint.py
@@ -0,0 +1,38 @@
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
+from flask_security import login_required, current_user
+import models 
+from app import db, user_datastore
+from sqlalchemy import text
+from .forms import UserForm
+
+
+task5 = Blueprint('task5', __name__, template_folder='templates')
+
+@task5.route('/task5')
+@login_required
+def index():
+    username = current_user.email
+    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        return jsonify({'username': username})
+    return render_template('task5/index.html')
+
+ 
+
+
+
+
+  
+
+
+
+
+   
+
+   
+
+
+
+
+
+
+    
