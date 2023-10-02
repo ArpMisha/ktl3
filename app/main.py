347b7208f3d59f2cb9065ea81227fb61ebbbc734
MikhailArpishkin <mixail.arpishkin@yandex.ru>
2023-10-02 10:06:38 +0300

    33

--- app/main.py
+++ app/main.py
@@ -4,13 +4,12 @@ from task.blueprint import task
 from task2.blueprint import task2
 from task3.blueprint import task3
 from task4.blueprint import task4
-from task5.blueprint import task5
 
 app.register_blueprint(task)
 app.register_blueprint(task2)
 app.register_blueprint(task3)
 app.register_blueprint(task4)
-app.register_blueprint(task5)
+
 
 if __name__ == '__main__':
 	app.run(host = '0.0.0.0')
