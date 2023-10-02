5d2ba99bcd79ebba920bfbdfab5fd7e1926ec800
MikhailArpishkin <mixail.arpishkin@yandex.ru>
2023-10-02 09:25:49 +0300

    55

--- app/task5/blueprint.py
+++ app/task5/blueprint.py
@@ -1,9 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, jsonify
-from flask_security import login_required, current_user
-from flask_security.forms import ChangePasswordForm
 import models 
 from app import db, user_datastore
-import pickle
+#import pickle
 
 
 task5 = Blueprint('task5', __name__, template_folder='templates')
@@ -12,25 +10,25 @@ task5 = Blueprint('task5', __name__, template_folder='templates')
 def index():
     return render_template('task5/index.html')
 
-@task5.route('/task5_upload', methods=['POST'])
-def upload_file():
-    try:
-        # Получаем JSON-данные из веб-формы
-        data = request.get_json()
-
-        if data:
-            # Сериализуем данные с помощью pickle
-            serialized_data = pickle.dumps(data)
+#@task5.route('/task5_upload', methods=['POST'])
+#def upload_file():
+#    try:
+#        # Получаем JSON-данные из веб-формы
+#        data = request.get_json()
+#
+#        if data:
+#            # Сериализуем данные с помощью pickle
+#            serialized_data = pickle.dumps(data)
             # Записываем сериализованные данные в файл
-            with open('data.pkl', 'wb') as file:
-                file.write(serialized_data)
+#            with open('data.pkl', 'wb') as file:
+#                file.write(serialized_data)
             # Отправляем файл обратно в веб-интерфейс
-            return send_file('data.pkl', as_attachment=True)
-        else:
-            return jsonify({'message': 'No JSON data received'})
+#            return send_file('data.pkl', as_attachment=True)
+#        else:
+#            return jsonify({'message': 'No JSON data received'})
 
-    except Exception as e:
-        return jsonify({'error': str(e)})
+#    except Exception as e:
+#        return jsonify({'error': str(e)})
 
 
 
