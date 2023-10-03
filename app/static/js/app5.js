1471eaf6b393534bf22cd6b78d28f81624c6e888
MikhailArpishkin <mixail.arpishkin@yandex.ru>
2023-10-03 17:03:38 +0300

    22

--- app/static/js/app5.js
+++ app/static/js/app5.js
@@ -1,44 +1,68 @@
-const { createApp } = Vue 
+document.addEventListener('DOMContentLoaded', function () {
+  // Initialize data
+  const search = {
+    kr_name: ''
+  };
+  const task = {
+    kr_name: '',
+    name: '',
+    kategor: '',
+    tip: ''
+  };
+  let tasks = [];
 
-const TaskApp = {
-  data(){
-    return {
-      tasks: {},
-      user: {
-        'name': '',
-        'surname': '',
-        'age': ''
-        }
-    }
-  },
-  async created(){
-    await this.getTasks()
-  },
-  methods: {
-    async getTasks(){
-      const response = await fetch(window.location, {
-        method: 'get',
-        headers: {
-          'X-Requested-With': 'XMLHttpRequest'
-        } 
-      })
-      this.tasks = await response.json()
-    },
-    async createTask(){
-      await this.getTasks()
-      const response = await fetch(window.location + '_upload', {
-        method: 'post',
-        headers: {
-          'Content-Type': 'application/json',
-          'X-Requested-With': 'XMLHttpRequest'
-        },
-        body: JSON.stringify(this.user)
-      })
-      await this.getTasks()
-    }
-    },
-    delimiters: ['{', '}']
+  // Helper function to update the task table
+  function updateTaskTable() {
+    const tableBody = document.querySelector('tbody');
+    tableBody.innerHTML = '';
+
+    tasks.forEach((task, index) => {
+      const row = tableBody.insertRow();
+      const cell1 = row.insertCell(0);
+      const cell2 = row.insertCell(1);
+      const cell3 = row.insertCell(2);
+      const cell4 = row.insertCell(3);
+
+      cell1.innerHTML = task.kr_name;
+      cell2.innerHTML = task.name;
+      cell3.innerHTML = task.kategor;
+      cell4.innerHTML = task.tip;
+    });
   }
-  
 
-createApp(TaskApp).mount('#app5')
+  // Event handler for the search form
+  const searchForm = document.querySelector('.card-search form');
+  searchForm.addEventListener('submit', async function (event) {
+    event.preventDefault();
+
+    const response = await fetch(window.location + '_search', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest'
+      },
+      body: JSON.stringify(search)
+    });
+
+    tasks = await response.json();
+    updateTaskTable();
+  });
+
+  // Event handler for the create form
+  const createForm = document.querySelector('.card-search:last-child form');
+  createForm.addEventListener('submit', async function (event) {
+    event.preventDefault();
+
+    const response = await fetch(window.location + '_create', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest'
+      },
+      body: JSON.stringify(task)
+    });
+
+    // You can handle the response as needed, e.g., display a success message.
+    // Optionally, you can update the tasks array and table if necessary.
+  });
+});
--- app/task5/blueprint.py
+++ app/task5/blueprint.py
@@ -1,44 +1,38 @@
 from flask import Blueprint, render_template, request, redirect, url_for, jsonify
+from flask_security import login_required
 import models 
-from app import db, user_datastore
-#import pickle
+from app import db
+from sqlalchemy import text
+from datetime import datetime
+from .forms import IsForm
+
 
 
 task5 = Blueprint('task5', __name__, template_folder='templates')
 
 @task5.route('/task5')
+#@login_required
 def index():
     return render_template('task5/index.html')
 
-#@task5.route('/task5_upload', methods=['POST'])
-#def upload_file():
-#    try:
-#        # Получаем JSON-данные из веб-формы
-#        data = request.get_json()
-#
-#        if data:
-#            # Сериализуем данные с помощью pickle
-#            serialized_data = pickle.dumps(data)
-            # Записываем сериализованные данные в файл
-#            with open('data.pkl', 'wb') as file:
-#                file.write(serialized_data)
-            # Отправляем файл обратно в веб-интерфейс
-#            return send_file('data.pkl', as_attachment=True)
-#        else:
-#            return jsonify({'message': 'No JSON data received'})
+@task5.route('/task5_search', methods=['POST'])
+#@login_required
+def task5_search():
+    user_input = request.get_json().get('kr_name')
+    results = models.iss.query.filter(models.iss.kr_name == user_input).all()
+    return jsonify(results)
 
-#    except Exception as e:
-#        return jsonify({'error': str(e)})
-
-
-
-
-
-
-    
-
-  
-
+@task5.route('/task5_create', methods=['POST'])
+#@login_required
+def task5_create():
+    user_input = request.get_json()
+    form = IsForm(data=user_input)
+    if form.validate():
+        task = models.iss(kr_name = form.kr_name.data, name = form.name.data, kategor = form.kategor.data, tip = form.tip.data)
+        db.session.add(task)
+        db.session.commit()
+        return jsonify(task)
+    return redirect(url_for('task5.index')) 
 
 
 
