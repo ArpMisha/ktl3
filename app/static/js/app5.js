3a35c621b2acecaa5bf1427ee393431b079b5c44
MikhailArpishkin <mixail.arpishkin@yandex.ru>
2023-10-04 12:07:03 +0300

    js add

--- app/static/js/app5.js
+++ app/static/js/app5.js
@@ -1,68 +1,87 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // Initialize data
-  const search = {
-    kr_name: ''
-  };
-  const task = {
-    kr_name: '',
-    name: '',
-    kategor: '',
-    tip: ''
-  };
-  let tasks = [];
-
-  // Helper function to update the task table
-  function updateTaskTable() {
-    const tableBody = document.querySelector('tbody');
-    tableBody.innerHTML = '';
-
-    tasks.forEach((task, index) => {
-      const row = tableBody.insertRow();
-      const cell1 = row.insertCell(0);
-      const cell2 = row.insertCell(1);
-      const cell3 = row.insertCell(2);
-      const cell4 = row.insertCell(3);
-
-      cell1.innerHTML = task.kr_name;
-      cell2.innerHTML = task.name;
-      cell3.innerHTML = task.kategor;
-      cell4.innerHTML = task.tip;
+document.addEventListener("DOMContentLoaded", function () {
+    // Находим форму по её ID
+    const form = document.getElementById("task-form");
+    // Добавляем обработчик события отправки формы
+    form.addEventListener("submit", function (e) {
+    e.preventDefault(); // Предотвращаем стандартное действие формы (отправку на сервер)
+    // Получаем значения полей формы
+    const krName = document.getElementById("task-kr_name").value;
+    const name = document.getElementById("task-name").value;
+    const kategor = document.getElementById("task-kategor").value;
+    const tip = document.getElementById("task-tip").value;
+    // Создаем JSON объект
+    const data = {
+        kr_name: krName,
+        name: name,
+        kategor: kategor,
+        tip: tip
+    };
+    // Преобразуем JSON объект в строку
+    const jsonData = JSON.stringify(data);
+    // Отправляем JSON данные на сервер с помощью Fetch API
+    fetch(window.location + '_create', {
+    method: 'POST', // Метод запроса
+    headers: {
+        'Content-Type': 'application/json' // Устанавливаем заголовок для JSON данных
+    },
+    body: jsonData // Передаем JSON данные как тело запроса
+    })
+    .then(response => response.json()) // Преобразуем ответ сервера в JSON формат
+    .then(data => {
+    console.log('Сервер ответил:', data);
+    // Здесь вы можете выполнить дополнительную обработку ответа сервера, если необходимо
+    })
+    .catch(error => {
+    console.error('Произошла ошибка:', error);
     });
-  }
-
-  // Event handler for the search form
-  const searchForm = document.querySelector('.card-search form');
-  searchForm.addEventListener('submit', async function (event) {
-    event.preventDefault();
-
-    const response = await fetch(window.location + '_search', {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Requested-With': 'XMLHttpRequest'
-      },
-      body: JSON.stringify(search)
     });
+});
 
-    tasks = await response.json();
-    updateTaskTable();
-  });
+document.getElementById("search-form").addEventListener("submit", function (e) {
+    e.preventDefault(); // Предотвращаем отправку формы по умолчанию
 
-  // Event handler for the create form
-  const createForm = document.querySelector('.card-search:last-child form');
-  createForm.addEventListener('submit', async function (event) {
-    event.preventDefault();
+    // Получаем значение поля ввода
+    var kr_name = document.getElementById("search-kr_name").value;
 
-    const response = await fetch(window.location + '_create', {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Requested-With': 'XMLHttpRequest'
-      },
-      body: JSON.stringify(task)
+    // Создаем объект JSON с данными
+    var data = {
+        kr_name: kr_name
+    };
+
+    // Отправляем POST-запрос
+    fetch(window.location + '_search', {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    .then(function (response) {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error("Ошибка сети");
+    })
+    .then(function (data) {
+        // Обработка успешного ответа и вывод данных в таблицу
+        var tableBody = document.getElementById("table-body");
+        tableBody.innerHTML = ""; // Очистка таблицы
+
+        data.forEach(function (item) {
+            var row = document.createElement("tr");
+            var keys = ["kr_name", "name", "kategor", "tip"];
+
+            keys.forEach(function (key) {
+                var cell = document.createElement("td");
+                cell.innerHTML = item[key];
+                row.appendChild(cell);
+            });
+
+            tableBody.appendChild(row);
+        });
+    })
+    .catch(function (error) {
+        // Обработка ошибок
+        console.error(error);
     });
-
-    // You can handle the response as needed, e.g., display a success message.
-    // Optionally, you can update the tasks array and table if necessary.
-  });
 });
